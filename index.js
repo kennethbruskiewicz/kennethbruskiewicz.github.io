738e1f9a39f2e435cf3fdeb05571e30d5ff004d3
Kenneth Bruskiewicz <kenneth.bruskiewicz@gmail.com>
2022-08-17 21:59:16 -0700

    recover index.js

--- index.js
+++ index.js
@@ -1,178 +1,26 @@
-<html>
-<head>
-<script src="https://cdn.jsdelivr.net/npm/3d-force-graph@1.70.10/dist/3d-force-graph.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/d3-dsv@3"></script>
-    <script src="data-set-loader.js"></script>
+const Graph = ForceGraph3D()
+	(document.getElementById("3d-graph"));
 
-    <link rel="stylesheet" href="style.css">
-</head>
+let curDataSetIdx,
+	numDim = 2;
 
-<body>
-    <div id="3d-graph"></div>
+const dataSets = getGraphDataSets();
 
-    <div class="dimensions-selector">
-        Dimensions:
-        <input type="radio" name="num-dimensions" onChange="toggleDimensions(1)"> 1D
-        <input type="radio" name="num-dimensions" onChange="toggleDimensions(2)" checked> 2D
-        <input type="radio" name="num-dimensions" onChange="toggleDimensions(3)"> 3D
-    </div>
-    <div class="graph-data">
-        <span id="graph-data-description"></span>
-        <button class="toggle-data-btn" onClick="toggleData()">Show me something else</button>
-    </div>
+let toggleData;
+(toggleData = function() {
+	curDataSetIdx = curDataSetIdx === undefined ? 0 : (curDataSetIdx+1)%dataSets.length;
 
-    <script src="index.js"></script>
-</body>
+	const dataSet = dataSets[curDataSetIdx];
+	document.getElementById('graph-data-description').innerHTML = dataSet.description ? `Viewing ${dataSet.description}` : '';
 
-</html>
+	dataSet(Graph
+		.resetProps()
+		.enableNodeDrag(false)
+		.numDimensions(numDim)
+	);
+})(); // IIFE init
 
-<!-- <!DOCTYPE html> -->
-<!-- <html> -->
-<!--   <head> -->
-<!--     <title>Sheets API Quickstart</title> -->
-<!--     <meta charset="utf-8" /> -->
-<!--   </head> -->
-<!--   <body> -->
-<!--     <p>Sheets API Quickstart</p> -->
-
-<!--     <\!--Add buttons to initiate auth sequence and sign out-\-> -->
-<!--     <button id="authorize_button" onclick="handleAuthClick()">Authorize</button> -->
-<!--     <button id="signout_button" onclick="handleSignoutClick()">Sign Out</button> -->
-
-<!--     <pre id="content" style="white-space: pre-wrap;"></pre> -->
-
-<!--     <script type="text/javascript"> -->
-<!--       /* exported gapiLoaded */ -->
-<!--       /* exported gisLoaded */ -->
-<!--       /* exported handleAuthClick */ -->
-<!--       /* exported handleSignoutClick */ -->
-
-<!--       // TODO(developer): Set to client ID and API key from the Developer Console -->
-<!--       const CLIENT_ID = '865060766020-2ba70eifqgbjl3cspnhbv2jse6ahvugv.apps.googleusercontent.com'; -->
-<!--       const API_KEY = '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'; -->
-
-<!--       // Discovery doc URL for APIs used by the quickstart -->
-<!--       const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4'; -->
-
-<!--       // Authorization scopes required by the API; multiple scopes can be -->
-<!--       // included, separated by spaces. -->
-<!--       const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'; -->
-
-<!--       let tokenClient; -->
-<!--       let gapiInited = false; -->
-<!--       let gisInited = false; -->
-
-<!--       document.getElementById('authorize_button').style.visibility = 'hidden'; -->
-<!--       document.getElementById('signout_button').style.visibility = 'hidden'; -->
-
-<!--       /** -->
-<!--        * Callback after api.js is loaded. -->
-<!--        */ -->
-<!--       function gapiLoaded() { -->
-<!--         gapi.load('client', intializeGapiClient); -->
-<!--       } -->
-
-<!--       /** -->
-<!--        * Callback after the API client is loaded. Loads the -->
-<!--        * discovery doc to initialize the API. -->
-<!--        */ -->
-<!--       async function intializeGapiClient() { -->
-<!--         await gapi.client.init({ -->
-<!--           apiKey: API_KEY, -->
-<!--           discoveryDocs: [DISCOVERY_DOC], -->
-<!--         }); -->
-<!--         gapiInited = true; -->
-<!--         maybeEnableButtons(); -->
-<!--       } -->
-
-<!--       /** -->
-<!--        * Callback after Google Identity Services are loaded. -->
-<!--        */ -->
-<!--       function gisLoaded() { -->
-<!--         tokenClient = google.accounts.oauth2.initTokenClient({ -->
-<!--           client_id: CLIENT_ID, -->
-<!--           scope: SCOPES, -->
-<!--           callback: '', // defined later -->
-<!--         }); -->
-<!--         gisInited = true; -->
-<!--         maybeEnableButtons(); -->
-<!--       } -->
-
-<!--       /** -->
-<!--        * Enables user interaction after all libraries are loaded. -->
-<!--        */ -->
-<!--       function maybeEnableButtons() { -->
-<!--         if (gapiInited && gisInited) { -->
-<!--           document.getElementById('authorize_button').style.visibility = 'visible'; -->
-<!--         } -->
-<!--       } -->
-
-<!--       /** -->
-<!--        *  Sign in the user upon button click. -->
-<!--        */ -->
-<!--       function handleAuthClick() { -->
-<!--         tokenClient.callback = async (resp) => { -->
-<!--           if (resp.error !== undefined) { -->
-<!--             throw (resp); -->
-<!--           } -->
-<!--           document.getElementById('signout_button').style.visibility = 'visible'; -->
-<!--           document.getElementById('authorize_button').innerText = 'Refresh'; -->
-<!--           await listMajors(); -->
-<!--         }; -->
-
-<!--         if (gapi.client.getToken() === null) { -->
-<!--           // Prompt the user to select a Google Account and ask for consent to share their data -->
-<!--           // when establishing a new session. -->
-<!--           tokenClient.requestAccessToken({prompt: 'consent'}); -->
-<!--         } else { -->
-<!--           // Skip display of account chooser and consent dialog for an existing session. -->
-<!--           tokenClient.requestAccessToken({prompt: ''}); -->
-<!--         } -->
-<!--       } -->
-
-<!--       /** -->
-<!--        *  Sign out the user upon button click. -->
-<!--        */ -->
-<!--       function handleSignoutClick() { -->
-<!--         const token = gapi.client.getToken(); -->
-<!--         if (token !== null) { -->
-<!--           google.accounts.oauth2.revoke(token.access_token); -->
-<!--           gapi.client.setToken(''); -->
-<!--           document.getElementById('content').innerText = ''; -->
-<!--           document.getElementById('authorize_button').innerText = 'Authorize'; -->
-<!--           document.getElementById('signout_button').style.visibility = 'hidden'; -->
-<!--         } -->
-<!--       } -->
-
-<!--       /** -->
-<!--        * Print the names and majors of students in a sample spreadsheet: -->
-<!--        * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit -->
-<!--        */ -->
-<!--       async function listMajors() { -->
-<!--         let response; -->
-<!--         try { -->
-<!--           // Fetch first 10 files -->
-<!--           response = await gapi.client.sheets.spreadsheets.values.get({ -->
-<!--             spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms', -->
-<!--             range: 'Class Data!A2:E', -->
-<!--           }); -->
-<!--         } catch (err) { -->
-<!--           document.getElementById('content').innerText = err.message; -->
-<!--           return; -->
-<!--         } -->
-<!--         const range = response.result; -->
-<!--         if (!range || !range.values || range.values.length == 0) { -->
-<!--           document.getElementById('content').innerText = 'No values found.'; -->
-<!--           return; -->
-<!--         } -->
-<!--         // Flatten to string to display -->
-<!--         const output = range.values.reduce( -->
-<!--             (str, row) => `${str}${row[0]}, ${row[4]}\n`, -->
-<!--             'Name, Major:\n'); -->
-<!--         document.getElementById('content').innerText = output; -->
-<!--       } -->
-<!--     </script> -->
-<!--     <script async defer src="https://apis.google.com/js/api.js" onload="gapiLoaded()"></script> -->
-<!--     <script async defer src="https://accounts.google.com/gsi/client" onload="gisLoaded()"></script> -->
-<!--   </body> -->
-<!-- </html> -->
+const toggleDimensions = function(numDimensions) {
+	numDim = numDimensions;
+	Graph.numDimensions(numDim);
+};
